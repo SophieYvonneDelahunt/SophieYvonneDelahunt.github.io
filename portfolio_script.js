6ff9d4933aad37e9eaa7d3d817f092e931cfb002
Sophie Delahunt <sophieyvonnedelahunt@gmail.com>
2026-02-18 23:20:56 -0500

    Further framework and styling

---
+++ portfolio_script.js
@@ -0,0 +1,3 @@
+window.addEventListener("load", function () {
+    let container = document.querySelector("#container"); // This is for drawing a cool firefly effect that I was unable to make functional before the assignment due date. I'll keep working on it.
+});
